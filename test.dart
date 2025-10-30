e99985b190ca3866790467264ebaed5d5faaef86
kimhy <kimhyyefla@gmail.com>
2025-10-30 15:00:03 +0900

    feat: Implement basic HomeScreen structure

--- test.dart
+++ test.dart
@@ -17,6 +17,12 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
 }
